caa48c222999885cca9bf955c6e176465b2d5174
cloudju <cloudju1978@gmail.com>
2020-08-08 10:42:30 +0900

    Initial Commit

---
+++ PrimeNumber/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PrimeNumber
+//
+//  Created by 居朝強 on 2020/08/08.
+//  Copyright © 2020 Cloud.Ju. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
